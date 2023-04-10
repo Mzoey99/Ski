8ea5022b140e0709984149efa6796e76e1917196
Marine Saint-Mezard <you@example.com>
2023-04-10 16:53:10 +0200

    ajout tests

---
+++ utils.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int in_liste(int data, int* liste, int n){
+    for (int i = 0; i < n-1; i++)
+    {
+        if (liste[i]==data)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+
+int max(int size, int* liste){
+    int maxi = liste[0];
+    for (int i = 0; i < size-1; i++)
+    {
+        if (liste[i]>maxi){
+            maxi = liste[i];
+        }
+    }
+    return maxi;
+}
+
+
+
+// int main(void)
+// {
+
+    
+
+//     return 0;
+// }
